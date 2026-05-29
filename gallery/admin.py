c5d5a87d7a84556e65b87149430d9b93d5f18100
Darek <jonskid44@gmail.com>
2026-05-29 22:07:24 +0200

    Added basic cms

---
+++ gallery/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Praca
+# Register your models here.
+admin.site.register(Praca)
---
+++ gallery/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Praca(models.Model):
+    SEMESTERS = [
+        ('sem3', 'Semestr 3'),
+        ('sem4', 'Semestr 4'),
+    ]
+
+    title = models.CharField(max_length=200, verbose_name="Tytuł pracy")
+    author = models.CharField(max_length=100, verbose_name="Autor (Imię i nazwisko)")
+    semester = models.CharField(max_length=10, choices=SEMESTERS, verbose_name="Semestr")
+    youtube_id = models.CharField(max_length=20, verbose_name="ID wideo z YouTube (np. WhWc3b3KhnY)")
+    description = models.TextField(verbose_name="Opis projektu")
+    date_added = models.DateTimeField(auto_now_add=True, verbose_name="Data dodania")
+    
+    def __str__(self):
+        return f"{self.title} - {self.author}"
+    
+    class Meta:
+        verbose_name = "Praca studencka"
+        verbose_name_plural = "Prace studenckie"
