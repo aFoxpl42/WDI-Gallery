9745519a817d67773e6591ece7e7bbf5d706b484
Darek <jonskid44@gmail.com>
2026-05-31 09:24:14 +0200

    Update .gitignore rules.

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,18 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+import os
+from pathlib import Path
+from dotenv import load_dotenv
 
+# Load the hidden variables from your .env file
+load_dotenv()
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
